8ba9377375fc6225e932ecebd39343a6eae0364c
parisiale <alessandro@puppetlabs.com>
2014-09-15 16:24:29 +0100

    Add new StringUtils::getExpiryDatetime().

--- src/common/string_utils.cpp
+++ src/common/string_utils.cpp
@@ -16,5 +16,25 @@ std::string StringUtils::plural<std::string>(std::vector<std::string> things) {
     return plural(things.size());
 }
 
+std::string StringUtils::getExpiryDatetime(int expiry_minutes) {
+    struct tm expiry_time_info;
+    char expiry_time_buffer[80];
+
+    // Get current time and add the specified minutes
+    time_t expiry_time { time(nullptr) };
+    expiry_time += 60 * expiry_minutes;
+
+    // Get local time structure
+    localtime_r(&expiry_time, &expiry_time_info);
+
+    // Return the formatted string
+    if (strftime(expiry_time_buffer, 80, "%FT%TZ", &expiry_time_info) == 0) {
+        // invalid buffer
+        return "";
+    }
+
+    return std::string { expiry_time_buffer };
+}
+
 }  // namespace Common
 }  // namespace Cthun
