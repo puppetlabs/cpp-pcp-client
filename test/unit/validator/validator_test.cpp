23ec7d2139885f363db7b6ba04d65918b183258b
Pieter Loubser <pieter.loubser@puppetlabs.com>
2015-02-27 17:47:26 +0000

    Change Validator to not be a singleton

--- test/unit/validator/validator_test.cpp
+++ test/unit/validator/validator_test.cpp
@@ -7,43 +7,43 @@
 namespace CthunClient {
 
 TEST_CASE("Validator::registerSchema", "[registerSchema]") {
-    Validator::Instance().reset();
+    Validator validator {};
     Schema schema {};
 
     SECTION("it can register a schema") {
-        Validator::Instance().registerSchema("test-schema", schema);
+        validator.registerSchema("test-schema", schema);
     }
 
     SECTION("it cannot register a schema name more than once") {
-        Validator::Instance().registerSchema("test-schema", schema);
-        REQUIRE_THROWS_AS(Validator::Instance().registerSchema("test-schema", schema),
+        validator.registerSchema("test-schema", schema);
+        REQUIRE_THROWS_AS(validator.registerSchema("test-schema", schema),
                           schema_redefinition_error);
     }
 }
 
 TEST_CASE("Validator::validate", "[validate]") {
-    Validator::Instance().reset();
     DataContainer data {};
     Schema schema {};
+    Validator validator {};
 
     SECTION("it throws on an invalid schema") {
-        REQUIRE_THROWS_AS(Validator::Instance().validate(data, "test-schema"),
+        REQUIRE_THROWS_AS(validator.validate(data, "test-schema"),
                           validation_error);
     }
 
     SECTION("it throws when validation fails") {
         data.set<std::string>("key", "value");
         schema.addConstraint("key", TypeConstraint::Int);
-        Validator::Instance().registerSchema("test-schema", schema);
-        REQUIRE_THROWS_AS(Validator::Instance().validate(data, "test-schema"),
+        validator.registerSchema("test-schema", schema);
+        REQUIRE_THROWS_AS(validator.validate(data, "test-schema"),
                           validation_error);
     }
 
     SECTION("it doesn't throw when validation succeeds") {
         data.set<std::string>("key", "value");
         schema.addConstraint("key", TypeConstraint::String);
-        Validator::Instance().registerSchema("test-schema", schema);
-        REQUIRE_NOTHROW(Validator::Instance().validate(data, "test-schema"));
+        validator.registerSchema("test-schema", schema);
+        REQUIRE_NOTHROW(validator.validate(data, "test-schema"));
     }
 
     SECTION("default schemas") {
@@ -55,28 +55,28 @@ TEST_CASE("Validator::validate", "[validate]") {
             data.set<std::vector<std::string>>("endpoints", endpoints);
             data.set<std::string>("data_schema", "envelope");
             data.set<bool>("destination_report", true);
-            REQUIRE_NOTHROW(Validator::Instance().validate(data, "envelope"));
+            REQUIRE_NOTHROW(validator.validate(data, "envelope"));
         }
 
         SECTION("it can validate an incorrectly formed envelope schema") {
             data.set<std::string>("id", "1234");
-        REQUIRE_THROWS_AS(Validator::Instance().validate(data, "envelope"),
-                          validation_error);        }
+        REQUIRE_THROWS_AS(validator.validate(data, "envelope"),
+                          validation_error);}
     }
 }
 
 TEST_CASE("Validator::getSchemaContentType", "[getSchemaContentType]") {
     Schema schema { ContentType::Binary };
-    Validator::Instance().reset();
+    Validator validator {};
 
     SECTION("it can return the schema's content type") {
-        Validator::Instance().registerSchema("test-schema", schema);
-        REQUIRE(Validator::Instance().getSchemaContentType("test-schema") ==
+        validator.registerSchema("test-schema", schema);
+        REQUIRE(validator.getSchemaContentType("test-schema") ==
                 ContentType::Binary);
     }
 
     SECTION("it throws when the schema is undefined") {
-        REQUIRE_THROWS_AS(Validator::Instance().getSchemaContentType("test-schema"),
+        REQUIRE_THROWS_AS(validator.getSchemaContentType("test-schema"),
                          schema_not_found_error);
     }
 }
