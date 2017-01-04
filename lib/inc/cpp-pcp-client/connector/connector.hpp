308a0c198de650ac8f6b8dc24fb7e97b641793a8
Michael Smith <michael.smith@puppet.com>
2017-01-04 11:59:18 -0800

    (maint) Add shims to preserve pxp-agent builds
    This work maintains compatibility with existing pxp-agent builds via
    several shims for headers moved to v1 namespaces.

---
+++ lib/inc/cpp-pcp-client/connector/connector.hpp
@@ -0,0 +1,12 @@
+/**
+ * This is a shim to maintain compatibility with pxp-agent during development.
+ */
+#pragma once
+
+#include <cpp-pcp-client/connector/v1/connector.hpp>
+
+namespace PCPClient {
+
+using namespace v1;
+
+}  // namespace PCPClient
