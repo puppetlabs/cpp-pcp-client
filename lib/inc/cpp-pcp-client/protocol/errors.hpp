83a5f3672c1ff7e429be1924373c7e5d12b0a327
Michael Smith <michael.smith@puppet.com>
2017-01-13 10:37:42 -0800

    (maint) Preserve header compatibility for library consumers
    Add shims for all headers moved to the v1 namespace to preserve header
    compatibility for library consumers.

---
+++ lib/inc/cpp-pcp-client/protocol/errors.hpp
@@ -0,0 +1,12 @@
+/**
+ * This is a shim to maintain compatibility with pxp-agent during development.
+ */
+#pragma once
+
+#include <cpp-pcp-client/protocol/v1/errors.hpp>
+
+namespace PCPClient {
+
+using namespace v1;
+
+}  // namespace PCPClient
