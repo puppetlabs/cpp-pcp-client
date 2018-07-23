100f6427efacb40e930c4633f489333bccf7c829
Casey Williams <caseywilliams@users.noreply.github.com>
2018-07-23 13:24:25 -0700

    (PA-1941) Update websocket++ from version 0.7.0 to 0.8.1
    For compatibility with OpenSSL 1.1.0

--- vendor/websocketpp.cmake
+++ vendor/websocketpp.cmake
@@ -4,8 +4,8 @@ INCLUDE(ExternalProject)
 EXTERNALPROJECT_ADD(
     websocketpp
     PREFIX "${PROJECT_BINARY_DIR}"
-    URL "file://${VENDOR_DIRECTORY}/websocketpp-0.7.0.zip"
-    URL_MD5 "1dfa1a91e48885e884589c6742fbf8b1"
+    URL "file://${VENDOR_DIRECTORY}/websocketpp-0.8.1.zip"
+    URL_MD5 "af0385e19ae5d37b087b1c73b176f10d"
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     BUILD_IN_SOURCE 1
