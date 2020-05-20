bbbdc4713cf962f3e36b2023501dc82dccae4752
Sean P. McDonald <sean.mcdonald@puppet.com>
2020-05-20 11:57:31 -0500

    (PA-3244) Update websocketpp for new boost
    This commit updates the vendored websocketpp library to 0.8.2 to take up
    changes required to upgrade boost.

--- vendor/websocketpp.cmake
+++ vendor/websocketpp.cmake
@@ -4,8 +4,8 @@ INCLUDE(ExternalProject)
 EXTERNALPROJECT_ADD(
     websocketpp
     PREFIX "${PROJECT_BINARY_DIR}"
-    URL "file://${VENDOR_DIRECTORY}/websocketpp-0.8.1.zip"
-    URL_MD5 "af0385e19ae5d37b087b1c73b176f10d"
+    URL "file://${VENDOR_DIRECTORY}/websocketpp-0.8.2.zip"
+    URL_MD5 "87d06d8ce62767602cb339d46b6664a1"
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     BUILD_IN_SOURCE 1
